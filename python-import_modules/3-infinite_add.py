f5afd9336316beef20660885e9a38452b3cf48fd
abrielleperry <abrielleperry22@icloud.com>
2023-09-07 21:03:31 -0700

    sum

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,4 +3,12 @@ if __name__ == "__main__":
     from sys import argv
     args = len(argv)
     sum = 0
+    if args == 1:
+        print("0")
+    elif args == 2:
+        print("{}".format(argv[1])i)
+    else:
+        for i in range(1, args):
+            sum += int(argv[i])
+            print("{}".format(sum))
 
