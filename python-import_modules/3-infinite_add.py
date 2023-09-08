72383be28891d8886927032187d3b498650c8c89
abrielleperry <abrielleperry22@icloud.com>
2023-09-08 14:56:25 -0700

    def main

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
+
+import sys
+
+
+def main():
+    total = 0
+    for arg in sys.argv[1:]:
+        total += int(arg)
+
+    print(total)
+
+
 if __name__ == "__main__":
-
-    import sys
-
-    args = len(sys.argv) - 1
-    if args == 0:
-        print("0 arguments.")
-    elif args == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(args))
-        for i in range(args):
-            sum += int(argv[i])
-            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    main()
