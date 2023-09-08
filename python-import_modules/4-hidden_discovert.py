273d17487edf32b865a5d666479d496d9c4fc64a
abrielleperry <abrielleperry22@icloud.com>
2023-09-07 21:32:18 -0700

    hidden

---
+++ python-import_modules/4-hidden_discovert.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import hidden_4
+if len(dir(hidden_4)) > 0:
+    for i in range(0, len(dir(hidden4))):
+        if dir(hidden_4)[i][0] != '_' and dir(hidden_4)[i][1] != '_':
+            print("{:s}".format(dir(hidden_4[i]))
+                    main()
