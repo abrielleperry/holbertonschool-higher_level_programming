f0909cdf396e204b70a888281233bea0c37c15c9
abrielleperry <abrielleperry22@icloud.com>
2023-09-08 09:22:46 -0700

    pysc fixes

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    
+
     import sys
 
-    args = len(sys:argv) - 1
+    args = len(sys.argv) - 1
     if args == 0:
         print("0 arguments.")
     elif args == 1:
