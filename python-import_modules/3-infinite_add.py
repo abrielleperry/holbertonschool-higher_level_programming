e1a86b247129c93022fa536009943c181a33f64b
abrielleperry <abrielleperry22@icloud.com>
2023-09-08 09:19:19 -0700

    changing else

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-from sys import argv
 if __name__ == "__main__":
-    args = len(argv)
-    sum = 0
-    if args == 1:
-        print("0")
-    elif args == 2:
-        print("{}".format(argv[1]))
+    
+    import sys
+
+    args = len(sys:argv) - 1
+    if args == 0:
+        print("0 arguments.")
+    elif args == 1:
+        print("1 argument:")
     else:
-        for i in range(1, args):
+        print("{} arguments:".format(args))
+        for i in range(args):
             sum += int(argv[i])
-            print("{}".format(sum))
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
