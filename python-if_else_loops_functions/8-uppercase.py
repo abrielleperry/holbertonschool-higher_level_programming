4b163412b0bfbbc71d873184ac759fa901a29e67
abrielleperry <abrielleperry22@icloud.com>
2023-09-07 18:02:49 -0700

    removing upper

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(97, 123):
-        if ord(c) == i:
-        print("{}".format(i).upper(), end='')
+    new = ""
+    for char in str:
+        if 97 <= ord(char) <= 122:
+            new = new + (chr(ord(char) - 32))
+        else:
+            new += char
+        print("{}".format(new))
