523c2c59239a8189dfa0046f6a874e7f3461c087
abrielleperry <abrielleperry22@icloud.com>
2023-09-07 20:14:34 -0700

    checker

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def function(Hello Welcome To The Best School):
+    
+    return len(argv)
+
+i = len(argv)
+if i < 2:
+    print("argument :")
+elif i >= 2:
+    print("arguments :")
+elif i == 0:
+    print(".")
