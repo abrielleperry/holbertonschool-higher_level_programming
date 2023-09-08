14857e523b7962bfbf0902e867acf514a5b6a53d
abrielleperry <abrielleperry22@icloud.com>
2023-09-07 21:54:33 -0700

    output y

--- python-import_modules/4-hidden_discovert.py
+++ python-import_modules/4-hidden_discovert.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+import hidden_4
 
-    import hidden_4
-if len(dir(hidden_4)) > 0:
-    for i in range(0, len(dir(hidden4))):
-        if dir(hidden_4)[i][0] != '_' and dir(hidden_4)[i][1] != '_':
-            print("{:s}".format(dir(hidden_4[i]))
-                    main()
+if __name__ == "__main__":
+    names = [name for name in dir(hidden_4) if not name.startswith("__")]
+for name in sorted(names):
+    print(name)
