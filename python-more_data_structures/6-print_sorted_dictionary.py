be1cc56c9f0e732deaa2c89c90b9866dd0668b32
abrielleperry <abrielleperry22@icloud.com>
2023-09-11 16:17:54 -0700

    change key

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    sorted_keys = dict(sorted(a_dictionary.key()))
+    sorted_keys = dict(sorted(a_dictionary.value()))
     for key in sorted_keys:
         print("{}: {})".format(key, sorted_keys[key]))
