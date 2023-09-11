ef9d91a6b218c19135288840af84215ca9d198bc
abrielleperry <abrielleperry22@icloud.com>
2023-09-11 15:44:27 -0700

    main

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    for i in range(0, len(my_list)):
+    for i in range(len(my_list)):
         if search == my_list[i]:
             new_list.append(replace)
         else:
---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    pres_elem = set_1.symmetric_difference(set_2)
+    return pres_elem
