5e26f0a335220a6a71874c9fa9c32bea453ce60e
abrielleperry <abrielleperry22@icloud.com>
2023-09-11 14:35:17 -0700

    list

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    for list in matrix:
+        listLen = len(list)
+        if listLen == 0:
+            print()
+            continue
+        new_list
