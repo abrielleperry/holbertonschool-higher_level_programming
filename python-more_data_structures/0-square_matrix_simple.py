3ef9636e14a5fd6062f252daec7c38278781093c
abrielleperry <abrielleperry22@icloud.com>
2023-09-11 14:38:16 -0700

    vari

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
+    new_matrix = []
     for list in matrix:
         listLen = len(list)
         if listLen == 0:
             print()
             continue
-        new_list
+        new_list = []
+        for num in list:
+            new_list.append(num ** 2)
+        new_matrix.append(new_list)
+    return new_matrix
