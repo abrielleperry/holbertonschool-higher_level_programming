68bf46cfc7be8485f111356d4adef3274c026b6f
abrielleperry <abrielleperry22@icloud.com>
2023-09-11 14:59:42 -0700

    still not working

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    my_list[search] = replace
+    new_list = []
+    for i in range(0, len(my_list)):
+        if search == my_list[i]:
+            new_list.append(replace)
+        else:
+            new_list.append(my_list[i])
     return my_list
