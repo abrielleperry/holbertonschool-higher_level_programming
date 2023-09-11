fcf8c501442bc01f3f14e6f551eccada2645832e
abrielleperry <abrielleperry22@icloud.com>
2023-09-11 11:11:30 -0700

    number to i

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for number in my_list:
-        print("{}".format(number))
+    for i in range(len(my_list) - 1, -1, -1):
+        print("{}".format(my_list[i]))
