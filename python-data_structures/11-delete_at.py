78aeb99b752cebff2aeccaaf5464125aa533adef
abrielleperry <abrielleperry22@icloud.com>
2023-09-11 14:04:18 -0700

    new list instead of delete

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
-        return my_list(idx)
-    del my_list[positioin]
-    return my_list
+        return my_list
+    new_list = my_list[:idx] + my_list[idx + 1:]
+    return new_list
 
