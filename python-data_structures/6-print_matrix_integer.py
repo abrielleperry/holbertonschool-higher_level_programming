06b7d2d213201ffff6e2582a17be17c9de1f5cc2
abrielleperry <abrielleperry22@icloud.com>
2023-09-11 12:36:19 -0700

    meeting checks

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,10 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in row:
-            print("{:d}".format(i), end=" ")
-        print()
+        list_len = len(row)
+        if list_len == 0:
+            print()
+            continue
+        for num in range(0, list_len - 1):
+            print("{:d}".format(row[num]), end=" ")
+        print("{:d}".format(row[list_len - 1]))
