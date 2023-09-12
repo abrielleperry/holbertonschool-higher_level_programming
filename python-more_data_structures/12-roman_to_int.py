cdc711947f93f66b92cf8571e0bfb9116946be0d
abrielleperry <abrielleperry22@icloud.com>
2023-09-12 15:31:22 -0700

    pycs fic

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,20 +2,17 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    
     rn_values = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
-    }
-    
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+            }
     result_int = 0
     previous_val = 0
-
     for numeral in reversed(roman_string):
         current_val = rn_values.get(numeral, 0)
         if current_val >= previous_val:
