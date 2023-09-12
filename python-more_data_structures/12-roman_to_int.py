1b744eafad9c8ed6595ea8bf9dfe9f22de44b783
abrielleperry <abrielleperry22@icloud.com>
2023-09-12 13:39:15 -0700

    chmod to test main

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string != str or None
-    return 0
+    for roman_string in range(U+2160, U+2188):
+        return number
+        if roman_string != str or None:
+        return 0
