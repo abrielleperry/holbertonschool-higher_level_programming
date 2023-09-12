a1773fb5f750aca20a497da044e11eddd73c2a01
abrielleperry <abrielleperry22@icloud.com>
2023-09-12 16:39:36 -0700

    current element in my list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,6 +3,9 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
+        for index in range(x):
+            if isinstance(mylist_[i], int):
+
     except:
 print("{:d}".format())
         
