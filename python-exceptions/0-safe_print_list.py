aed28df944084ef06fcacc9fff2f0c3174f0b9da
abrielleperry <abrielleperry22@icloud.com>
2023-09-12 16:47:28 -0700

    return count

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,10 @@ def safe_print_list(my_list=[], x=0):
     try:
         for index in range(x):
             if isinstance(mylist_[i], int):
-
+                print("{:d}".format(my_list[i], end="\n"))
+                count += 1
     except:
-print("{:d}".format())
-        
+    return count
+
+
+
