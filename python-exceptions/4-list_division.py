181da033619fb02dd7a05d0b1dedde1a20ab64d1
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2023-09-13 14:54:07 -0500

    change len() to list_length

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,15 +2,15 @@
 def list_division(my_list_1, my_list_2, list_length):
     
     new_list = []
-    for i in range(0, len(my_list_1)):
+    for i in range(0, list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
             new_list.append(result)
-        except TypeError:
-            print("wrong type")
-            new_list.append(0)
         except ZeroDivisionError:
             print("division by 0")
+            new_list.append(0)
+        except TypeError:
+            print("wrong type")
             new_list.append(0)
         except IndexError:
             print("out of range")
@@ -18,6 +18,3 @@ def list_division(my_list_1, my_list_2, list_length):
     if len(new_list) < list_length:
             new_list.append(0)
     return new_list
-        
-
-
