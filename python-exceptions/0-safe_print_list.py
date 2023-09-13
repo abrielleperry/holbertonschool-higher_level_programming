9948e48ba24da6fe956a6a0bde16c94aae6ed78e
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2023-09-13 12:21:15 -0500

    correcting inside result inside print format

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            if isinstance(my_list[i]):
+            if isinstance(my_list[i], ):
                 print("{:d}".format(my_list[i], end="\n"))
                 count += i
     except (IndexError, TypeError):
