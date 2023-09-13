c65f86bd9ec8d0f030ddffd3c1426e7b6a57b458
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2023-09-13 10:38:07 -0500

    changing return to x

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,9 +4,9 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            if isinstance(my_list[i], int):
+            if isinstance(my_list[i]):
                 print("{:d}".format(my_list[i], end="\n"))
-                count += 1
+                count += i
     except (IndexError, TypeError):
         pass
-    return i
+    return x
