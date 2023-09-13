1fc2bb861b42b6eb98d3be9a752b30b67f8040a0
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2023-09-13 14:39:35 -0500

    missing out of range

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,3 +1,23 @@
 #!/usr/bin/Python3
 def list_division(my_list_1, my_list_2, list_length):
+    
+    new_list = []
+    for i in range(0, len(my_list_1)):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+            new_list.append(result)
+        except TypeError:
+            print("wrong type")
+            new_list.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            new_list.append(0)
+        except IndexError:
+            print("out of range")
+            new_list.append(0)
+    if len(new_list) < list_length:
+            new_list.append(0)
+    return new_list
+        
+
 
