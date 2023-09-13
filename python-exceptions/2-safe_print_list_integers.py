fed95236b03b4b18f56b9abe9279ec922b5625c4
abrielleperry <abrielleperry22@icloud.com>
2023-09-12 18:02:46 -0700

    increment i and count

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,3 +7,10 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             i += 1
         except ValueError:
+            i += 1
+            continue
+        except TypeError:
+            i += 1
+            continue
+        print()
+        return count
