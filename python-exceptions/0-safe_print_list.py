63982a5efb898dc0f751e5ec33195ed8cd1467a7
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2023-09-13 20:51:25 -0500

    put int back in and took end out as not needed

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,9 +4,9 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            if isinstance(my_list[i], ):
-                print("{:d}".format(my_list[i], end="\n"))
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]))
                 count += i
     except (IndexError, TypeError):
         pass
-    return x
+    return count
