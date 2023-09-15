81273dc4adc6869281b1818256c23773d6cf926e
Abrielle Perry <Abrielle@Zacharys-iMac.local>
2023-09-15 13:26:40 -0500

    initionalizing square with size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,6 +3,7 @@
 that defines a square based on 0-square.py"""
 
 class Square:
-    def __init__(self, size, Square):
-      self.__size = self
-"""self and double underscore is used to make size a private instance attribue"""
+    """emply class to define Square"""
+    def __init__(self, size):
+        self.__size = size
+        """initinalizing Square with size"""
