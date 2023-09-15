21e44ee6014e9a4647263f0aa1633d685e9eb4eb
Abrielle Perry <Abrielle@Zacharys-iMac.local>
2023-09-15 14:11:19 -0500

    changing if to elif

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,11 +4,11 @@
 class Square:
     """empty class to define Square"""
     def __init__(self, size=0):
-        if isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+            self.__size = size
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
 
     def area(self):
         return self.__size ** 2
