a41c868a4a43e65dc1dc0167f9d65a4101572d74
abrielleperry <abrielleperry22@icloud.com>
2023-09-18 11:41:06 -0700

    changing size to value in if statements and adding returns

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,21 +1,26 @@
 #!/usr/bin/Python3
-"""This file will write a class Square that defines a square based on 3-sqaure.py"""
+"""This file will write a class Square defining a square based on 3-sqaure.py"""
 
 class Square:
-    """empty class to define Square"""
+    """initializing a Square object"""
     def __init__(self, size=0):
+        self.__size = size
 
     @property
-    def size(self)
-    
-    @value.setter
-    def size(self, value)
-        if not isinstance(size, int):
+    def size(self):
+        """getting side length of Square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setting side length of Square"""
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
-    
+        self.__size = value
+
     def area(self):
-        return (self.__size * self.__size)
+        """returning area of square which is side length squared"""
+        return self.__size ** 2
 
