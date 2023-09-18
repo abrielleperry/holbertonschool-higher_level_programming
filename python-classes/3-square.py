832a2b55798ab8ea2a62b4f712f71b697c0d8004
abrielleperry <abrielleperry22@icloud.com>
2023-09-18 09:39:07 -0700

    missing second area

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -4,12 +4,12 @@
 class Square:
     """empty class to define Square"""
     def __init__(self, size=0):
-        self.__size = size
-        if not isinstance(size, int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must >= 0")
-            
-        def area(self):
-            return self.__size ** 2
-"""returns area of current square"""
+        self.__size = size            
+
+    def area(self):
+        return self.__size ** 2
+    """returns area of current square"""
