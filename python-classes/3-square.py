8eb788fe412f435ecf90cce698cf65a719bbe97a
Abrielle Perry <Abrielle@Zacharys-iMac.local>
2023-09-18 10:21:21 -0500

    chmod +x *.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,15 +1,20 @@
 #!/usr/bin/Python3
 """"this file writes a Square that defines a square by 2-square.py"""
 
+
 class Square:
-    """empty class to define Square"""
+
     def __init__(self, size=0):
+        """empty class to define Square"""
         self.__size = size
-        if not isinstance(size, int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must >= 0")
-            
-        def area(self):
-            return self.__size ** 2
-"""returns area of current square"""
+
+
+
+
+    def area(self):
+        """returns area of current square"""
+        return self.__size**2
