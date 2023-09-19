8b32698f0e7c278217d38f5423136d35bf0cb692
abrielleperry <abrielleperry22@icloud.com>
2023-09-19 13:19:10 -0700

    fixing pycs

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -2,6 +2,7 @@
 """this is a file that will write  a function that prints
 a square with the character #"""
 
+
 def print_square(size):
     """print_square function take int and prints size of square with hash"""
     if not isinstance(size, (int, float)):
@@ -10,7 +11,7 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     if size < 0:
         raise TypeError("size must be an integer")
-    
+
     square = "#" * size
     for _ in range(size):
         print("{}".format(square))
