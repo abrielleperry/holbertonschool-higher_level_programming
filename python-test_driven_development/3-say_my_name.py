bac65f4952e601b794cc9e8682ad35552189468c
abrielleperry <abrielleperry22@icloud.com>
2023-09-19 12:21:11 -0700

    fixing doc string

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,8 +1,10 @@
 #!/usr/bin/Python3
-""" """
+"""this file will write a function that prints My name is with 
+the first name and last name"""
 
 
 def say_my_name(first_name, last_name=""):
+    """say_my_name will return string of first and last name"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
