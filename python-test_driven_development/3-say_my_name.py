272c345f6f49dc61bfee71c519c1442af85b9516
abrielleperry <abrielleperry22@icloud.com>
2023-09-19 11:53:13 -0700

    testing

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,10 @@
+#!/usr/bin/Python3
+""" """
+
+
+def say_my_name(first_name, last_name=""):
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    return first_name + "" + last_name
