b1845cb067afb6b3e67b9fb92505e9d0daa5fb1e
abrielleperry <abrielleperry22@icloud.com>
2023-09-19 13:41:39 -0700

    outpput looks correct

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/Python3
+"""this file writes a function that prints a text with 2 new
+lines after each of these characters: ., ? and :"""
+
+
+def text_indentation(text):
+    """text_identation prints a text with 2 new lines"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    text = text.replace(".", ".\n \n")
+    text = text.replace("?", "?\n \n")
+    text = text.replace(":", ":\n \n")
+
+    print(text)
