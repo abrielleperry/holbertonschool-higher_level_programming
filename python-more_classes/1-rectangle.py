215184be63a0e9d81d3ae0bf299372b1ea9e4655
abrielleperry <abrielleperry22@icloud.com>
2023-09-20 07:46:47 -0700

    fixing pyc

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/Python3
+"""file that creates class Rectangle"""
+
+
+class Rectangle:
+    """empty class Rectangle"""
+    def __init__(self, width=0, height=0):
+        """initilizing rectangle with width and height"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """getting width of recantangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setting side value of rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getting height of rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setting height of rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
