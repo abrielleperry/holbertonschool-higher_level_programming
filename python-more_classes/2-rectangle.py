7d0a9d389d932b6c003ddfbc3a821329e7438529
abrielleperry <abrielleperry22@icloud.com>
2023-09-20 09:07:31 -0700

    trying to fix int not callable error

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/Python3
-"""file that creates class Rectangle"""
+"""file that creates class Rectangle based on 1-rectangle.py"""
 
 
 class Rectangle:
@@ -21,7 +21,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+            self.__height = value
 
     @property
     def width(self):
@@ -36,3 +36,12 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
+    def area(self):
+        self.area = self.__width * self.__height
+        return self.area
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
