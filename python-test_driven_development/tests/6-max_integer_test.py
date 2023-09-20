0de41798d48e4250d3c8c335ab8eed6080e229ec
abrielleperry <abrielleperry22@icloud.com>
2023-09-19 19:19:44 -0700

    sudo 3 files

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/Python3
+"""unicode for max_function that returns max int from list"""
+
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+import pycodestyle
+
+class TestMaxInteger(unittest.TestCase):
