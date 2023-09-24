2c9211163ec567b4c6b58eb208cd38725c2d077e
abrielleperry <abrielleperry22@icloud.com>
2023-09-24 11:05:42 -0700

    sudo .py

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/Python3
+""" Write a class MyList that inherits from list
+"""
+class Mylist(list):
+    pass
+
+    def print_sorted(self):
+        """class mylist that inherites from list"""
+        int = self
+        print (int.sort())
