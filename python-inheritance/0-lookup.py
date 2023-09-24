f706c2fe84f532d824b397069ec77c67b7f454f8
abrielleperry <abrielleperry22@icloud.com>
2023-09-24 10:25:15 -0700

    trying item for intem with dir

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/Python3
+"""this file creates a function that returns
+a list of available attributes and methods of an object"""
+
+
+def lookup(obj):
+    """getting list of available attributes and methods of an object
+
+    Args:
+        obj (object): the object to retrieve attribures and methods
+
+    Returns:
+        list: list of available attributes and methods of an object
+    """
+    return [item for item in dir(obj) if not item.startswith("__")]
