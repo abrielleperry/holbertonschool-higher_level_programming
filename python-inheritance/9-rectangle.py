9eca8569077e092c42bcc710a68c60e61ccc37fd
abrielleperry <abrielleperry22@icloud.com>
2023-09-25 13:04:29 -0700

    creating importing chmod

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/Python3
+"""a class Rectangle that inheritates from BaseGeometry"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """a class reping a rectangle with private width and heoghth attributes"""
+
+    def __init__(self, width, height):
+        """initalizing a rectangle with private width and heoghth attributes
+
+        Args:
+            width (int)
+            height (int)
+        """
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
