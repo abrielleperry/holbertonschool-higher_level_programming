e9c576dbee7108b1327f035a263e9b50e0de05c4
abrielleperry <abrielleperry22@icloud.com>
2023-09-25 10:05:56 -0700

    create class based on 5.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/Python3
+"""a module that creates a class BaseGeometry based on 5-base_geometry.py"""
+
+
+class BaseGeometry:
+    """_summary_
+    """
+    def area(self):
+        """_summary_
+
+        Raises:
+            Exception: _description_
+        """
+        raise Exception("area() is not implemented")
