44ac7611eb038fd9d59dbd47b976f80a72962d17
abrielleperry <abrielleperry22@icloud.com>
2023-09-25 09:43:00 -0700

    type is not was also needed to remove output bool

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -14,4 +14,4 @@ def inherits_from(obj, a_class):
     Returns: True if the object is an instance of a class that inherited
     (directly or indirectly) from the specified class, otherwise False.
     """
-    return isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) is not a_class
