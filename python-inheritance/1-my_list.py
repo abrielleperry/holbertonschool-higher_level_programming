cfac1bb1f817b705963ff92e4de041146aacc470
abrielleperry <abrielleperry22@icloud.com>
2023-09-25 08:04:07 -0700

    chmod 2 main

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/Python3
 """ Write a class MyList that inherits from list
 """
+
+
 class Mylist(list):
-    pass
+    """"""
 
     def print_sorted(self):
         """class mylist that inherites from list"""
-        int = self
-        print (int.sort())
+        print(sorted(self))
---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/Python3
+
+
+def is_same_class(obj, a_class):
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+        a_class (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return type(obj) is a_class
