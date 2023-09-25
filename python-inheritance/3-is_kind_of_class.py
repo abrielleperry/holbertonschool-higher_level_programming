ed215ff137321e692874382d63e14880a4f58308
abrielleperry <abrielleperry22@icloud.com>
2023-09-25 09:01:29 -0700

    trying isinstance and type

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/Python3
+"""a module that defines is_kind_of_class function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """check if obj is an instance of or if the object is an instance
+    of a class that inherited from the specified class
+
+        Args:
+            obj (object): obejct to check
+            a_class (type): specified class to check against
+
+        Returns:
+            bool: True if obj is an instance of or if the object is an instance
+        of a class that inherited from the specified class, otherwise False
+        """
+        return if isinstance(obj, a_class) and type(obj) is a_class
