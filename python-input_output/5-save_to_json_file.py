0cd606378866f894fe9a8e9341897af242e0b708
abrielleperry <abrielleperry22@icloud.com>
2023-09-26 13:52:16 -0700

    shortened without actal syns from 3 and 4

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,7 +6,4 @@ import json
 
 def save_to_json_file(my_obj, filename):
     with open(filename, "w", encoding="utf-8") as file:
-        objtotext = file.write(text)
-        json_objtotext = json.loads(my_obj)
-    return json_objtotext
-    
+        json.dump(my_obj, file)
