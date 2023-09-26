2f87a8f91833e47aaa536a1efcb82648d9e5da58
abrielleperry <abrielleperry22@icloud.com>
2023-09-26 13:16:06 -0700

    import json

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,5 +1,7 @@
 #!/usr/bin/Python3
 """Writes a function that returns the JSON representation of an object (string)"""
-
+import json
 
 def to_json_string(my_obj):
+    json_str = json.dumps(my_obj)
+    return json_str
