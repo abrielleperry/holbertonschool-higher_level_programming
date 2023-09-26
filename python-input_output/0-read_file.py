e117876bffd783b55090ec8428ffbd6b6ff04267
abrielleperry <abrielleperry22@icloud.com>
2023-09-26 11:22:00 -0700

    adding docstring

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,13 @@
 #!/usr/bin/Python3
+    """a function that reads a text file (UTF8) and prints it to stdout
+    """
 
 def read_file(filename=""):
+    """reads text file
+
+    Args:
+        filename (str, optional): Defaults to "".
+    """
     with open(filename, encoding="utf-8") as f:
         for line in f:
             print(line, end="")
