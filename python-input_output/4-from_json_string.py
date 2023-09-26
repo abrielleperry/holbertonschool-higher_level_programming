0cf7e083da618403501b116d7832bad15c584a3a
abrielleperry <abrielleperry22@icloud.com>
2023-09-26 13:37:51 -0700

    added doc string

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -5,5 +5,13 @@ import json
 
 
 def from_json_string(my_str):
+    """returns object to represented jsonstring
+
+    Args:
+        my_str (str): json string
+
+    Returns:
+        json_obj : object represented by json strong
+    """
     json_obj = json.loads(my_str)
     return json_obj
