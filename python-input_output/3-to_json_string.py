25cc106b271500c461e853c27d78f08a8ffbe142
abrielleperry <abrielleperry22@icloud.com>
2023-09-26 13:20:20 -0700

    added docstring

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -3,5 +3,13 @@
 import json
 
 def to_json_string(my_obj):
+    """function representing an object
+
+    Args:
+        my_obj (obj): reprentation of object
+
+    Returns:
+        json_str (str): json string
+    """
     json_str = json.dumps(my_obj)
     return json_str
