6094d824380e3f42b8868747c11a40a237cff7b7
abrielleperry <abrielleperry22@icloud.com>
2023-09-26 13:54:37 -0700

    docstring

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,5 +5,11 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """function writing object to txt file using json rep
+
+    Args:
+        my_obj: obj written to txt file
+        filename: specified txt file
+    """
     with open(filename, "w", encoding="utf-8") as file:
         json.dump(my_obj, file)
