81043be1fd6d142e870af8703b40eb114f9e887c
abrielleperry <abrielleperry22@icloud.com>
2023-09-26 11:04:04 -0700

    rm inout from root

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/Python3
 
 def read_file(filename=""):
-    with open(my_file_0, mode=w, encoding=utf-8) as 0-read_file:
-        print(my_file_0.read())
+    with open(filename, encoding="utf-8") as f:
+        print(file.read(), end="")
+        file.close()
