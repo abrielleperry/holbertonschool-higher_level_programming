12cb43f9580496f54b3720095e05136c7802c3b4
abrielleperry <abrielleperry22@icloud.com>
2023-09-26 12:26:58 -0700

    sudo for 2s

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a function that appends a string at the end of
+a text file (UTF8) and returns the number of characters added"""
+
+
+def append_write(filename="", text=""):
+    with open(filename, encoding="utf-8") as file:
+        for line(end="") in file:
+            print(append.line)
