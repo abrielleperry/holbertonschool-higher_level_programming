a69f9e17dfb2249e5710f4ecfbcacd8f16a5b1a5
abrielleperry <abrielleperry22@icloud.com>
2023-09-26 12:05:17 -0700

    add docstring

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,6 +4,15 @@
 
 
 def write_file(filename="", text=""):
+    """writes text to file w specified file name
+
+    Args:
+        filename (str): file to write to
+        text (str): text being written to file
+
+    Returns:
+        numchars (int): number of characters
+    """
     with open(filename, "w", encoding="utf-8") as file:
         numchars = file.write(text)
     return numchars
