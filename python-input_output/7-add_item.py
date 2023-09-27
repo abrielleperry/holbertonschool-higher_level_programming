b92c0a18b40aceee6b4d94246101a60bd69a9e34
abrielleperry <abrielleperry22@icloud.com>
2023-09-26 18:54:23 -0700

    no out

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,13 +6,13 @@ from sys import argv
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
+try:
+    load_from_json_file("add_item.json")
 
-for add_item.json:
-    my_list = load_from_json_file(add_item.json)
-while:
-    my_list = []
+except Exception as e:
+    list = []
 
-for ard in sys.argv[1:]:
-    my_list.append(arg)
+for obj in argv[1:]:
+    list.append(obj)
 
-    save_to_json_file(my_list, add_item.json)
+    save_to_json_file(list, "add_item.json")
