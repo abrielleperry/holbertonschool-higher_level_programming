f273f5408eb0a209b202299fdd098a77d9373d8e
abrielleperry <abrielleperry22@icloud.com>
2023-09-26 17:56:54 -0700

    sudo

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,2 +1,13 @@
 #!/usr/bin/Python3
 """Write a function that creates an Object from a “JSON file”"""
+import json
+
+def load_from_json_file(filename):
+    with open(filename, "r", encoding="utf-8") as file:
+        cont_json = file.read()
+    data = filename.loads(cont_json)
+
+class objfrmjson:
+    def __init__(filename, data):
+
+        ObjJson = objfrmjson(data)
