997dbc2f55f8463213c5d5a986e4b361ef78a5d0
abrielleperry <abrielleperry22@icloud.com>
2023-09-27 12:38:46 -0700

    pyc fixes

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,7 +4,7 @@ to a Python list, and then save them to a file"""
 import json
 from sys import argv
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file= __import__('5-save_to_json_file').save_to_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 try:
     load_from_json_file("add_item.json")
