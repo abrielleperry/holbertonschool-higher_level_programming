fb343aaa1666d274c72996673fe4979c9a2f6bb6
abrielleperry <abrielleperry22@icloud.com>
2023-09-26 18:30:45 -0700

    appendiung

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,3 +5,14 @@ import json
 from sys import argv
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+for add_item.json:
+    my_list = load_from_json_file(add_item.json)
+while:
+    my_list = []
+
+for ard in sys.argv[1:]:
+    my_list.append(arg)
+
+    save_to_json_file(my_list, add_item.json)
