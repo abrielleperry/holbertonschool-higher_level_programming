3e90275d9bb6e57fe1fad622413f8a80a39f8156
abrielleperry <abrielleperry22@icloud.com>
2023-09-27 15:34:08 -0700

    arg

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,15 +3,16 @@
 to a Python list, and then save them to a file"""
 import json
 from sys import argv
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
 
 try:
-    load_from_json_file("add_item.json")
+    content = load("add_item.json")
 
-except Exception:
-    new_list = []
+except FileNotFoundError:
+    content = []
 
-for obj in new_list[:]:
-    new_list.append(obj)
-    save_to_json_file(new_list, "add_item.json")
+for arg in argv[1:]:
+    content.append(arg)
+
+    save(content, "add_item.json")
