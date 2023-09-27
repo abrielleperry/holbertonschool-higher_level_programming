ceb08c8ece1952cfa2334603936620a636b411a9
abrielleperry <abrielleperry22@icloud.com>
2023-09-26 19:27:46 -0700

    doc string

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -3,10 +3,18 @@
 
 
 class Student(first_name, last_name, age):
+    """initialing public instance attributes
+
+    Args:
+        first_name 
+        last_name 
+        age 
+    """
     def __init__(self, first_name, last_name, age):
         self.first_name
         self.last_name = last_name
         self.age = age
 
         def to_json(self):
+            """returns dictionary representation of a Student instance"""
             return self.__dict__
