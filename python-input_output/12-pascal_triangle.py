f6fc2f6309773dd0207349759339864ebed6d2ce
abrielleperry <abrielleperry22@icloud.com>
2023-09-26 20:11:14 -0700

    triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -12,14 +12,14 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    tri = []
+    triangle = []
     for i in range(n):
         row = [1]
         if i > 0:
-            prev = tri[i - 1]
+            prev = triangle[i - 1]
             for j in range(1, i):
                 row.append(prev[j - 1] + prev[j])
             row.append(1)
-        tri.append(row)
+        triangle.append(row)
 
-    return tri
+    return triangle
