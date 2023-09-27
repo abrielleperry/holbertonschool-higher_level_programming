fba2168a4541e8c31b4b5db1aff29f787b60f483
abrielleperry <abrielleperry22@icloud.com>
2023-09-27 09:56:31 -0700

    without e

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,16 +3,16 @@
 to a Python list, and then save them to a file"""
 import json
 from sys import argv
-load = __import__('6-load_from_json_file').load_from_json_file
-save = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file= __import__('5-save_to_json_file').save_to_json_file
 
 try:
-    load("add_item.json")
+    load_from_json_file("add_item.json")
 
-except Exception as e:
+except Exception:
     new_list = []
 
 for obj in argv[1:]:
     new_list.append(obj)
 
-    save(new_list, "add_item.json")
+    save_to_json_file(new_list, "add_item.json")
