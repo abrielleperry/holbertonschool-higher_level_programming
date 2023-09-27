398a584c87caae283c22571df02c01ba5afb4ed2
abrielleperry <abrielleperry22@icloud.com>
2023-09-27 12:21:14 -0700

    reverting to yesterday

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,7 +9,7 @@ save_to_json_file= __import__('5-save_to_json_file').save_to_json_file
 try:
     load_from_json_file("add_item.json")
 
-except Exception:
+except Exception as e:
     new_list = []
 
 for obj in argv[1:]:
