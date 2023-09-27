392826f79e3a493d0abb5628a385330118f5b9f5
abrielleperry <abrielleperry22@icloud.com>
2023-09-26 18:17:36 -0700

    unable to get value error

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -2,12 +2,8 @@
 """Write a function that creates an Object from a “JSON file”"""
 import json
 
+
 def load_from_json_file(filename):
     with open(filename, "r", encoding="utf-8") as file:
-        cont_json = file.read()
-    data = filename.loads(cont_json)
-
-class objfrmjson:
-    def __init__(filename, data):
-
-        ObjJson = objfrmjson(data)
+        cont_json = json.load(file)
+    return (cont_json)
