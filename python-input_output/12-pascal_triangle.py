b7bf388730ba49d3526a2b39cc6d203101e74dda
abrielleperry <abrielleperry22@icloud.com>
2023-09-26 19:59:05 -0700

    out of range

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
-    """ """
+"""that returns a list of lists of integers
+representing the Pascal’s triangle of n"""
 
 
-    def pascal_triangle(n):
-        if n <= 0:
-            return []
-        tri = []
-        for i in range(n):
-            element_row = [1]
-            if 1 > 0:
-                prev = tri[1 - 1] 
-
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    tri = []
+    for i in range(n):
+        element_row = [1]
+        if 1 > 0:
+            prev = tri[i - 1]
+            for e in range(1, i):
+                    row.append[1]
+            tri.append(row)
         return tri
