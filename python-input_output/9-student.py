b0c521fbf90085de8a48bcadb71d0227bfde779c
abrielleperry <abrielleperry22@icloud.com>
2023-09-26 19:31:48 -0700

    forgot parts of defining and tojson was overindex

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -2,19 +2,19 @@
 """Write a class Student that defines a student """
 
 
-class Student(first_name, last_name, age):
+class Student:
     """initialing public instance attributes
 
     Args:
-        first_name 
-        last_name 
-        age 
+        first_name
+        last_name
+        age
     """
     def __init__(self, first_name, last_name, age):
-        self.first_name
+        self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-        def to_json(self):
+    def to_json(self):
             """returns dictionary representation of a Student instance"""
             return self.__dict__
