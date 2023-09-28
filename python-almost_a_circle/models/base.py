5249d88dbdb12acbd12b6de014d4f0f056e1f1d4
abrielleperry <abrielleperry22@icloud.com>
2023-09-28 13:39:37 -0700

    pyc fixes

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,22 +5,20 @@ template for creating other subclasses
 
 
 class Base:
+    """ keeps track of number of objects created from this class """
     __nb_objects = 0
-"""
-     keeps track of number of objects created from this class
-"""
 
-def __init__(self, id=None):
-    """
-        __init__:
-        Initializes new instance of Base class
+    def __init__(self, id=None):
+        """
+            __init__:
+            Initializes new instance of Base class
 
-        Args:
-        id (int): Int value set as unique id
-        if not provided set to number of objects created
-    """
-    if id is not None:
+            Args:
+            id (int): Int value set as unique id
+            if not provided set to number of objects created
+        """
+        if id is not None:
             self.id = id
-    else:
-        Base.__nb_objects += 1
-        self.id = Base.__nb_objects
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
