2f87b001a781a632e3a564091bd707d5a1f10eb9
abrielleperry <abrielleperry22@icloud.com>
2023-09-28 11:55:53 -0700

    increment happens before value change

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,10 +2,12 @@
 """ """
 
 
-class Base(__nb_objects = 0):
+class Base:
+    __nb_objects = 0
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
-            self.id = Base.__nb_objects 
             Base.__nb_objects += 1
+            self.id = Base.__nb_objects
