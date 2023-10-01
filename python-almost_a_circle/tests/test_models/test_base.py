a5a099b2d067127db227df30aa73e371b82307f8
abrielleperry <abrielleperry22@icloud.com>
2023-10-01 08:00:17 -0700

    fixed typos in tests

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -2,15 +2,16 @@
 
 import unittest
 from models.base import Base
+import pycodestyle
 
 
 class TestBase(unittest.TestCase):
 
     def test_module_docstring(self):
-        self.assertTrue(len(__import__("modules").base.__doc__) > 0)
+        self.assertTrue(len(__import__("models").base.__doc__) > 0)
 
     def test_class_docstring(self):
-        self.assertTrue(len(__import__("modules").base.Base.__doc__))
+        self.assertTrue(len(__import__("models").base.Base.__doc__))
 
     def test_isinstance(self):
         b2 = Base()
@@ -24,3 +25,8 @@ class TestBase(unittest.TestCase):
     def test_input_with_id(self):
         b3 = Base(12)
         self.assertEqual(b3.id, 12)
+
+    def test_pycodestyle(self):
+        style = pycodestyle.StyleGuide(quiet=True)
+        checkPyC = style.check_files(["models/base.py"])
+        self.assertEqual(checkPyC.total_errors, 0, "Found code style errors (and warnings)")
