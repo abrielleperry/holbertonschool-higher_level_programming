6ae7af0c441375bd6c94e3d194e5ec13b746cf7d
abrielleperry <abrielleperry22@icloud.com>
2023-10-01 16:53:41 -0700

    chnage from private to public

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -17,10 +17,10 @@ class Rectangle(Base):
             y (int): y coordinate to rectangle
             id (int): id of rectangle
         """
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
