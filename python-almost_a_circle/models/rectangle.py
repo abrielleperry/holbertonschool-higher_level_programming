d0c0dbbccd0f9bcf470e0364846eae1c230a48b4
abrielleperry <abrielleperry22@icloud.com>
2023-10-01 14:21:33 -0700

    add docstring

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,6 +7,16 @@ from models.base import Base
 class Rectangle(Base):
     """call superclass constructer with id"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        initializes new instance of Rectangle class
+
+        Args:
+            width (int): width of rectangle
+            height (int): height of rectangle
+            x (int): x coordinate to rectangle
+            y (int): y coordinate to rectangle
+            id (int): id of rectangle
+        """
         self.__width = width
         self.__height = height
         self.__x = x
@@ -16,23 +26,23 @@ class Rectangle(Base):
     def get_width(self):
         return self.__width
     
+    def set_width(self, width):
+        self.__width = width
+    
     def get_height(self):
         return self.__height
     
+    def set_height(self, height):
+        self.__height = height
+    
     def get_x(self):
         return self.__x
+    
+    def set_x(self, x):
+        self.__x = x
 
     def get_y(self):
         return self.__y
     
-    def set_width(self, width):
-        self.__width = width
-
-    def set_height(self, height):
-        self.__height = height
-
-    def set_x(self, x):
-        self.__x = x
-
     def set_y(self, y):
         self.__y = y
