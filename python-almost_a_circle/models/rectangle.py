ba609d59ac9de16fa8e83ffc6122d62210ba4779
abrielleperry <abrielleperry22@icloud.com>
2023-10-01 16:44:27 -0700

    greater than

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -25,13 +25,13 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """ get width of rectangle """
+        """ get_width of rectangle """
         return self.__width
 
     @width.setter
     def width(self, value):
         """
-        set width of rectangle
+        set_width of rectangle
 
         Args:
             width (int): new width of value
@@ -39,7 +39,7 @@ class Rectangle(Base):
             TypeError: if value is not integer
             ValueError: if value is less than or equal to 0
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -47,13 +47,13 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """get height of rectangle"""
+        """get_height of rectangle"""
         return self.__height
 
     @height.setter
     def height(self, value):
         """
-        set height of rectangle
+        set_height of rectangle
 
         Args:
             height (int): new height value
@@ -62,7 +62,7 @@ class Rectangle(Base):
             TypeError: if value is not integer
             ValueError: if value is less than or equal to 0
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -70,22 +70,22 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        """get x coordinate of rectangle"""
+        """get_x coordinate of rectangle"""
         return self.__x
 
     @x.setter
     def x(self, value):
         """
-        set x coordinate of rectangle
+        set_x coordinate of rectangle
 
         Args:
             x (int): new x coordinate
-
+        
         Raises:
             TypeError: if value is not integer
             ValueError: if value is less than or equal to 0
         """
-        if type(value) is not int:
+        if not isinstance (value, int):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -93,13 +93,13 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        """get y coordinate of rectangle"""
+        """get_y coordinate of rectangle"""
         return self.__y
 
     @y.setter
     def y(self, value):
         """
-        set y coordinate of rectangle
+        set_y coordinate of rectangle
 
         Args:
             y (int): new y coordinate of rectangle
@@ -108,8 +108,8 @@ class Rectangle(Base):
             TypeError: if value is not integer
             ValueError: if value is less than or equal to 0
         """
-        if type(value) is not int:
+        if not isinstance (value, int):
             raise TypeError("y must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
