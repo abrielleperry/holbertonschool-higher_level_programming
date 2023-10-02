79e870884ed387f7c62c1e8c742e6b5a991dbc3e
abrielleperry <abrielleperry22@icloud.com>
2023-10-02 12:30:38 -0700

    mv getter and setter from rec add docstring

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -37,3 +37,48 @@ class Square(Rectangle):
         """
         self.width = value
         self.height = value
+
+    @property
+    def width(self):
+        """ get_width of square """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        set_width of square
+
+        Args:
+            width (int): new width of value
+        Raises:
+            TypeError: if value is not integer
+            ValueError: if value is less than or equal to 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """get_height of rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        set_height of square
+
+        Args:
+            height (int): new height value
+
+        Raises:
+            TypeError: if value is not integer
+            ValueError: if value is less than or equal to 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
