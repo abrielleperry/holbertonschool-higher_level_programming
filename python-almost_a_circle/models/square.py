0b1cf5a30cac9957f533eef13ba804fdfe337df5
abrielleperry <abrielleperry22@icloud.com>
2023-10-02 12:13:36 -0700

    size setter

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -10,10 +10,16 @@ class Square(Rectangle):
         super().__init__(self, size, x, y, id)
 
     def __str__(self):
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {size}"
-    
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
     @property
     def size(self):
         return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+    
     
     
