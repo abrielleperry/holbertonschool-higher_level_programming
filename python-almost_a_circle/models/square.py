21351f30e256b8d9129a7f381143568f430be6e2
abrielleperry <abrielleperry22@icloud.com>
2023-10-02 12:11:20 -0700

    inheriting from rec changing str and getting size of sq

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,4 +6,14 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(self, size, x, y, id)
+
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {size}"
+    
+    @property
+    def size(self):
+        return self.width
+    
     
