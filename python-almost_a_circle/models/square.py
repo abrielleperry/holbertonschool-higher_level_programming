f0475fa49d57ad1a583ab355b30eec7b0104b09e
abrielleperry <abrielleperry22@icloud.com>
2023-10-02 12:20:51 -0700

    doc string added

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,20 +6,34 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """class representing square
+    """
     def __init__(self, size, x=0, y=0, id=None):
+        """initializing new instance of square class
+
+        Args:
+            size (int): size of square
+            x (int): x coordinate of square
+            y (int): y coordinate of square
+            id (int): id of square
+        """
         super().__init__(self, size, x, y, id)
 
     def __str__(self):
+        """string representation of square"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     @property
     def size(self):
+        """size gets size of square"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """setting size for square
+
+        Args:
+            value (int): new size value
+        """
         self.width = value
         self.height = value
-    
-    
-    
