f516220e579cfeb6cc3c737101c4690b5b2c2fd4
abrielleperry <abrielleperry22@icloud.com>
2023-10-02 12:36:21 -0700

    mv args and kwargs to sq and rm wid and hig w size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -82,3 +82,21 @@ class Square(Rectangle):
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
+
+    def update(self, *args, **kwargs):
+        """update attributes that are assigned to key/value arguments
+        """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            self.id = kwargs.get("id", self.id)
+            self.size = kwargs.get("size", self.size)
+            self.x = kwargs.get("x", self.x)
+            self.y = kwargs.get("y", self.y)
