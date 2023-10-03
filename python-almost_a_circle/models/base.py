b8c76b8ab0784e2667feb66a723ecfec96b890d2
abrielleperry <abrielleperry22@icloud.com>
2023-10-03 11:37:16 -0700

    from json string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -57,3 +57,7 @@ class Base:
             file.write(json_string)
 
     def from_json_string(json_string):
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)
