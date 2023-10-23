1b040c702bce8ec96f646c1f11c5d902a3756ca5
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2023-10-23 15:54:07 -0500

    IF NOT

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- creates database and table with unique, not null, autoincrment, primary key
-CREATE DATABASE hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE states(
+CREATE TABLE IF NOT EXISTS states(
   id INT AUTO_INCREMENT NOT NULL,
   PRIMARY KEY (id),
   name VARCHAR(256) NOT NULL
