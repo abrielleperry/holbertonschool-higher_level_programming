b37a3b6b7d0e541d288641021b222d9a3493f25a
root <abrielleperry22@icloud.com>
2023-10-24 19:29:27 -0700

    hardy har

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,2 +1,33 @@
 #!/usr/bin/python3
-""""""
+"""takes in an argument and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument"""
+
+
+import MySQLdb
+from sys import argv
+
+
+def state_name_searched():
+    """takes 4 args: username, password, database and state_name_searched"""
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        database=argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE BINARY name = '{}'"
+        .format(argv[4])
+    )
+    all_data = cursor.fetchall()
+    for state in all_data:
+        print(state)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    state_name_searched()
