d8348adeaa17755b5c87377c32e645651eeb632e
root <abrielleperry22@icloud.com>
2023-10-24 17:57:48 -0700

    pyc fixes

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""lists all states with a name starting with N"""
+
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        database=argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute(
+      "SELECT * FROM states WHERE name"
+      "LIKE '%N' ORDER BY states.id"
+    )
+    all_data = cursor.fetchall()
+    for row in all_data:
+        print(row)
