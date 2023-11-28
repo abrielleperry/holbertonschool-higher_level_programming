5792a3a87c8426d33f98e47629bf2e195460efaa
root <abrielleperry22@icloud.com>
2023-11-28 15:21:59 -0800

    take arg length

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
+if (process.argv.length <= 3) {
+  console.log(0)
+} else {
+  const arg1 = process.argv(2)(Number);
+  arg1
+  console.log(arg1[1]);
 
-process.argv.length
-console.log()
+}
