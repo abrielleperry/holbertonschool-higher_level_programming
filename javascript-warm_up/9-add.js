86f5586f2df14743034c5d6b32d8c92c35a1071a
root <abrielleperry22@icloud.com>
2023-11-28 15:06:11 -0800

    checks nan

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
-function add(a, b) {
+function add (a, b) {
+  if (isNaN(a) || isNaN(b)) {
+    return NaN;
+  }
   return a + b;
 }
-const arg1 = parseInt
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+console.log(add(arg1, arg2));
