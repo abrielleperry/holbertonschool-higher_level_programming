13b5ca2ff014da2af23df5cab04d31071a1722d3
root <abrielleperry22@icloud.com>
2023-11-28 11:09:28 -0800

    takes index 3

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-console.log();
-'My number: <first argument converted in integer></first>'
-process.argv
+const arg1 = process.argv[2];
+if arg1 === int;
+console.log('My number:' + arg1);
