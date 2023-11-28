ba94b0f70480eb0241c9f1a3809242846afa2693
root <abrielleperry22@icloud.com>
2023-11-28 12:06:16 -0800

    const

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 while
-“C is fun”
-“Python is cool”
-“JavaScript is amazing”
-console.log()
+const myVar1 = 'C is fun'
+const myVar2 = 'Python is cool'
+const myVar3 = 'JavaScript is amazing'
+console.log(myVar1 + myVar2 + myVar3)
