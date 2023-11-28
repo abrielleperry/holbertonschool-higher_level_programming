abe7f1412b0a52279e8b1150089021eb07850335
root <abrielleperry22@icloud.com>
2023-11-28 15:16:00 -0800

    factorial

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
-
-NaN
-1
+function factorial (i) {
+  if (isNaN(i) || i === 0) {
+    return 1;
+  } else {
+    return i * factorial(i - 1);
+  }
+}
 const int = parseInt(process.argv[2]);
 console.log(factorial(int));
