b4a2d8630baa6b11e1cb9543537a42793d8dbeb2
root <abrielleperry22@icloud.com>
2023-11-28 15:25:29 -0800

    slice

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) {
-  console.log(0)
+  console.log(0);
 } else {
-  const arg1 = process.argv(2)(Number);
-  arg1
+  const arg1 = process.argv.slice(2).map(Number);
+  arg1.sort((a, b) => b - a);
   console.log(arg1[1]);
-
 }
