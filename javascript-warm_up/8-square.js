40367d1da7aca7eb2b3fb6ce45d3c4cc2e98c8f1
root <abrielleperry22@icloud.com>
2023-11-28 13:04:13 -0800

    rm )

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const X = parseInt(process.argv[2]);
-if (isNaN)(X)) {
+if (isNaN(X)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < ResizeObserverSize; i++) {
-  console.log('X'.repeat(X));
+  for (let i = 0; i < X; i++) {
+    console.log('X'.repeat(X));
   }
 }
