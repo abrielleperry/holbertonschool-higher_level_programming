2f4ce37705451efe0feeec519168a9aa832c2414
root <abrielleperry22@icloud.com>
2023-11-28 11:20:38 -0800

    semi fixes

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
 const arg1 = process.argv[2];
-if arg1 === int;
-console.log('My number:' + arg1);
+const int = Number(arg1);
+
+if (isNaN(int)) {
+  console.log('Not a number');
+} else {
+  console.log('My number:' + int);
+}
