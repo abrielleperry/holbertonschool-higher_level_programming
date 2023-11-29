02ab62226a030e25e3138a89bb82392b4995b06e
root <abrielleperry22@icloud.com>
2023-11-29 14:58:33 -0800

    exports

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,17 +1,3 @@
 #!/usr/bin/node
-const Rectangle = require('./5-square');
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
-  charPrint (c) {
-    if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
-    }
-  }
-}
-module.exports = Square;
+exports.nbOccurences = function (list, searchElement)
+module.exports = nbOccurences;
