d7d376fa202dab81184ea2ef65ccab689c70dfb9
root <abrielleperry22@icloud.com>
2023-11-29 14:16:34 -0800

    change to let

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -8,5 +8,10 @@ class Rectangle {
       Object.assign(this, {});
     }
   }
+print() {
+  for (let i = 0; i < this.height; i++) {
+    console.log('X'.repeat(this.width));
+    }
+  }
 }
 module.exports = Rectangle;
