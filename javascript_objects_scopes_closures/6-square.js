67d1003d217137dfd202bbeff259a91d94e41176
root <abrielleperry22@icloud.com>
2023-11-29 14:46:08 -0800

    semi fixes

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -5,10 +5,13 @@ class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
 }
 module.exports = Square;
-
-charPrint(c)
-c
-X
-undefined
