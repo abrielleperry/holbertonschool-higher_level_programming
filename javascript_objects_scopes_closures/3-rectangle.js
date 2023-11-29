50c2a61d19d7b52983189d72b23a8191c220a36a
root <abrielleperry22@icloud.com>
2023-11-29 14:12:12 -0800

    2-rec

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      Object.assign(this, {});
+    }
   }
 }
+module.exports = Rectangle;
