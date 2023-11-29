0e30c49e19395b84fd1068b2519e21e76aae390e
root <abrielleperry22@icloud.com>
2023-11-29 14:39:53 -0800

    file 6

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const Rectangle = require('./5-square');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+module.exports = Square;
+
+charPrint(c)
+c
+X
+undefined
